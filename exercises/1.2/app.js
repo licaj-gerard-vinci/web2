5b6230332f909ea3bc8554965ef960d03f020fa6
Gerard <gerard.licaj@student.vinci.be>
2023-09-19 22:19:04 +0200

    1.2 : app middleware

--- exercises/1.2/app.js
+++ exercises/1.2/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { log } = require('console');
 
 var app = express();
 
@@ -12,41 +13,22 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-var countGET = 0;
-var countGETpizzas = 0;
-var countPOSTpizzas = 0;
-var countDELETEpizzas = 0;
+const stats = {};
 
 app.use((req, res, next) => {
-
-    if (req.method == "GET" && req.path == '/') {
-        console.log(req.path);
-        countGET++;
-    }
-
-    if (req.method == "GET" && req.path == "/pizzas") {
-        console.log(req.path);
-        countPOSTpizzas++;
-    }
-
-    if (req.method == "POST" && req.path == "/pizzas") {
-        console.log(req.path);
-        countPOSTpizzas++;
-    }
-
-    if (req.method == "DELETE" && req.path == "/pizzas") {
-        console.log(req.path);
-        countPOSTpizzas++;
-    }
-
-    console.log('-GET / :', countGET);
-    console.log('-GET /pizzas :', countGETpizzas);
-    console.log('-POST /pizzas :', countPOSTpizzas);
-    console.log('-DELETE /pizzas :', countDELETEpizzas);
+const currentOperation  = `- ${req.method} ${req.path}`;
+const currentOperationCounter = stats[currentOperation];
+if (currentOperationCounter === undefined) stats[currentOperation] = 0;
+stats[currentOperation]++;
+console.log("Request counter: ");
+for(key in stats) {
+    console.log(`${key} : ${stats[key]}`);
+}
     next();
   });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
